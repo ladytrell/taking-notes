6d0b2ffb47e4f17153016822373843dbf4bea146
Lady Trell <antrellkent@gmail.com>
2022-04-27 16:41:55 -0400

    add delete route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -6,7 +6,15 @@ const notes = require('../../db/db.json');
 // ID generator
 const idgen = require('idgen');
 
-// Confirm content are strings
+// Confirm note content are strings
+function validateID(id) {    
+    if (!id || typeof id !== 'string' || id.length != 6) {
+      return false;
+    }
+    return true;
+}
+
+// Confirm note content are strings
 function validate(note) {
     if (!note.title || typeof note.title !== 'string') {
       return false;
@@ -14,11 +22,13 @@ function validate(note) {
     if (!note.text || typeof note.text !== 'string') {
       return false;
     }
-    if (!note.id || typeof note.id !== 'string') {
+    if (!validateID(note.id)) {
       return false;
     }
+    
     return true;
-  }
+}
+
 // Route to a single note by id
 router.get('/notes/:id', (req, res) => {
     let result = {};
@@ -60,6 +70,34 @@ router.post('/notes', (req, res) => {
     let results = notes;
     
     res.json(results);
+});
+
+// Route to add a note
+router.delete('/notes/:id', (req, res) => {
+    const deletedID = req.params.id;
+    let editedArray = []; 
+
+    if (!validateID(deletedID)) {
+        res.status(400).send("Note is formatted properly.")
+    } else {
+        editedArray = notes.filter(note => note.id != deletedID);
+        //Overwrite db.json with new array
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            // null-include all properties of the array and use 2 spaces for indentions
+            JSON.stringify(editedArray, null, 2)
+         );       
+    }  
+    
+    notes.forEach(() => {
+        notes.pop();
+    });
+    
+    editedArray.forEach(note => {
+        notes.push(note);
+    });
+
+    res.json(notes);
 });
 
 module.exports  = router;
