9a4944006567cd9208a2d1657338dc927d4eaa27
Lady Trell <antrellkent@gmail.com>
2022-04-27 20:35:54 -0400

    delete a note

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const router = require('express').Router();
 // Notes database (JSON file)
-const notes = require('../../db/db.json');
+let notes = require('../../db/db.json');
 // ID generator
 const idgen = require('idgen');
 
@@ -75,27 +75,19 @@ router.post('/notes', (req, res) => {
 // Route to add a note
 router.delete('/notes/:id', (req, res) => {
     const deletedID = req.params.id;
-    let editedArray = []; 
 
     if (!validateID(deletedID)) {
         res.status(400).send("Note is formatted properly.")
     } else {
-        editedArray = notes.filter(note => note.id != deletedID);
+        notes = notes.filter(note => note.id != deletedID);
+        
         //Overwrite db.json with new array
         fs.writeFileSync(
             path.join(__dirname, '../../db/db.json'),
             // null-include all properties of the array and use 2 spaces for indentions
-            JSON.stringify(editedArray, null, 2)
+            JSON.stringify(notes, null, 2)
          );       
-    }  
-    
-    notes.forEach(() => {
-        notes.pop();
-    });
-    
-    editedArray.forEach(note => {
-        notes.push(note);
-    });
+    }
 
     res.json(notes);
 });
